fffaa5f8d730b6e6cc7fb3c5240c45b80986cdb5
Student account <sandu.diaconu@code4000.internal>
2022-12-30 13:13:04 +0200

    chat from max backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,72 @@
 import "./App.css";
 import io from "socket.io-client";
-import { useState } from "react";
-import Chat from "./Chat";
-
-const socket = io.connect("http://localhost:3001");
+import { useState, useEffect } from "react";
+const socket = io.connect("http://localhost:3001")
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [room, setRoom] = useState("");
-  const [showChat, setShowChat] = useState(false);
 
-  const joinRoom = () => {
-    if (username !== "" && room !== "") {
-      socket.emit("join_room", room);
-      setShowChat(true);
-    }
-  };
+    const [username, setUsername] = useState("");
+    const [room, setRoom] = useState("");
+    const [msg, setMsg] = useState("");
+    const [userList, setUserList] = useState([]);
 
-  return (
-    <div className="App">
-      {!showChat ? (
-        <div className="joinChatContainer">
-          <h3>Join A Chat</h3>
-          <input
-            type="text"
-            placeholder="John..."
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
-          />
-          <input
-            type="text"
-            placeholder="Room ID..."
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-          />
-          <button onClick={joinRoom}>Join A Room</button>
+    // Register an username to server db:
+    const login = () => {
+        if (username !== "") {
+            socket.emit("login", username);
+            socket.emit('msg','request')
+        } else {
+            console.log('Set your username.')
+        }
+    };
+ 
+    // Display Online:
+    socket.on('online',(data)=>{
+      console.log(data)
+    });
+
+    // Send a message:
+    function sendMsg() {
+        if (room !== "") {
+            const message = {
+                msg: msg,
+                room: room
+            };
+            socket.emit("send_message", message)
+        } else {
+            console.log("Username or ID is missing")
+        }
+
+    };
+
+    // Display msg:
+    useEffect(() => {
+        socket.on('receive_message', (data) => {
+
+            console.log(data)
+
+        })
+    }, [socket]);
+    
+    return ( 
+    <div className = "App" >
+
+        <input type = "text" placeholder = "Your Name" onChange = {(event) => {setUsername(event.target.value);}}/>
+
+        <button onClick = { login }> Set Your Username </button>
+
+        <div>
+          <p>Users online: </p>
         </div>
-      ) : (
-        <Chat socket={socket} username={username} room={room} />
-      )}
-    </div>
-  );
-}
 
-export default App;
+        <input type = "text" placeholder = "Client/Group name" onChange = {(event) => {setRoom(event.target.value);}}/>
+
+
+        <input type = "text" placeholder = "Message" onChange = {(event) => {setMsg(event.target.value);}}/>
+
+        <button onClick = { sendMsg } > Send Msg </button>
+
+        </div>)
+    }
+
+    export default App
---
+++ server/server.js
@@ -0,0 +1,80 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require('cors');
+const { Server } = require('socket.io');
+app.use(cors());
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+let db = new Array(); // Contain full details from each user(nickname/id).
+let users = new Set(); // Contain all online users name(only nickanme).
+
+
+io.on('connection', (socket) => {
+
+    // Send a message to UI:
+    socket.on('send_message', (data) => {
+        console.log(data)
+        socket.to(data.room).emit('receive_message', data.msg);
+    });
+
+    // Register and Update new connected client:' save in "db" and "users" '.
+    socket.on('login', (data) => {
+        const user = {};
+        user.nickname = data;
+        user.id = socket.id;
+
+        if (db.length === 0) {
+            db.push(user);
+            users.add(data);
+        } else if (db.length > 0) {
+            for (nickname of db) {
+                users.add(nickname.nickname);
+                if (nickname.nickname === data) {
+                    nickname = socket.id
+                }
+            };
+
+            let allUsers = Array.from(users);
+            let value = allUsers.find(val => {
+                return val === data
+            });
+            if (!value) {
+                db.push(user);
+                users.add(data);
+            };
+        };
+        console.log({ db, users });
+        display();
+
+    });
+
+
+    // Send online users to UI:
+    function display() {
+        socket.leave(socket.id);
+        socket.broadcast.emit('online', JSON.stringify(Array.from(db)));
+        socket.join(socket.id);
+    };
+
+    // Remove from db any disconnected user:
+    socket.on('disconnect', () => {
+        for (i = 0; i < db.length; i++) {
+            if (db[i].id === socket.id) {
+                users.delete(db[i].nickname);
+                db.splice(i, 1);
+            };
+        };
+        console.log({ db, users });
+        display();
+    });
+});
+
+
+io.listen(3001, () => {
+    console.log("Server is online");
+});
